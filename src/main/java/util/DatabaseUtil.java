9200cc2d7aeb9e97e63bdb3cedbdb48199f5479a
Hossein Sadeghi <hossein.sadeghi.offcial@gmail.com>
2023-03-05 16:45:10 +0330

    add createTable method to DatabaseUtil

--- src/main/java/util/DatabaseUtil.java
+++ src/main/java/util/DatabaseUtil.java
@@ -3,8 +3,21 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseUtil {
+
+    public static void createTable() throws SQLException{
+        try(Connection conn = getConnection();
+        Statement stm = conn.createStatement()){
+            String createUserTableSql = "CREATE TABLE IF not EXISTS users (id SERIAL PRIMARY KEY, username VARCHAR(50) NOT NULL, national_code VARCHAR(10) NOT NULL, birthday DATE NOT NULL, password VARCHAR(50) NOT NULL)";
+            stm.executeUpdate(createUserTableSql);
+            String createArticleTableSql = "CREATE TABLE IF not EXISTS articles (id INTEGER DEFAULT NEXTVAL('article_id_seq') PRIMARY KEY, title VARCHAR(50) NOT NULL, content TEXT NOT NULL, user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE)";
+            stm.executeUpdate(createArticleTableSql);
+        }
+    }
+
+
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:postgresql://localhost:5432/article_management";
         String user = "postgres";
