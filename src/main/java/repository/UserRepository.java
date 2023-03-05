7a8c1480b91ec632144053427c930b93a614ff96
Hossein Sadeghi <hossein.sadeghi.offcial@gmail.com>
2023-03-05 16:42:40 +0330

    create UserRepository and add addUser Method (test OK)

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,35 @@
+package repository;
+
+import entity.User;
+import util.DatabaseUtil;
+
+import java.sql.*;
+
+public class UserRepository {
+
+    public void addUser(User user) throws SQLException {
+        String sql = "INSERT INTO users (username, national_code, birthday, password) VALUES (?, ?, ?, ?)";
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+        ) {
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getNationalCode());
+            ps.setDate(3, user.getBirthday());
+            ps.setString(4, user.getPassword());
+
+            int rowInserted = ps.executeUpdate();
+
+            if (rowInserted == 0) {
+                throw new SQLException("Creating user failed, no rows affected");
+            }
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    int id = generatedKeys.getInt(1);
+                    user.setId(id);
+                } else {
+                    throw new SQLException("creating user failed, no ID obtained.");
+                }
+            }
+        }
+    }
+}
